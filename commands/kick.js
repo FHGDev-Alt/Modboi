940ef7b000f8c15d8b7806206677fac5d5e5bbb5
FHGDev <gideon.lingle14@gmail.com>
2018-05-05 18:37:26 -0500

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,21 @@
+module.exports.run = (bot, message, args) => {
+  let member = message.mentions.members.first();
+  if (!member) return message.channel.send("You need to mention someone.");
+  let em = new discord.RichEmbed()
+  .setTitle("Modboi Kicking")
+  .setTimestamp()
+  .setColor(`RANDOM`)
+  if (reason) {
+    em.setDescription(`BOOM! ${member.user.username} is outta here! Reason: ${reason}`);
+    member.kick(reason).catch(err => console.error(err))
+    message.channel.send({embed: em});
+  } else {
+    em.setDescription(`BOOM! ${member.user.username} is outta here!`);
+    member.kick().catch(err => console.error(err))
+    message.channel.send({embed: em})
+  }
+}
+
+module.exports.help = {
+    name: "kick"
+}
