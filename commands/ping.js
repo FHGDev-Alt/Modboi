7330bc391ce5851d198717e9e7b97b5dbd48b5d5
FHGDev <gideon.lingle14@gmail.com>
2018-05-05 09:34:25 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,12 @@
+let discord = require('discord.js')
+
 module.exports.run = (bot, message, args) => {
   let em = new discord.RichEmbed()
-  .setTitle("")
+  .setTitle("Modboi Ping")
   .setDescription(`PONG! My ping is ${Math.floor(bot.ping)}ms!`)
   .setTimestamp()
+  .setColor("RANDOM")
+  message.channel.send({embed: em})
 }
 
 module.exports.help = {
